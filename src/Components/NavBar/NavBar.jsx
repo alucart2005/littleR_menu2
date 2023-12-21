98f897c637eba9be364a5ac4c143ea06f1e6adf3
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-12-21 18:15:46 -0500

    Feature: ERROR

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,72 @@
+import { Link } from "react-router-dom";
+import { IconContext } from "react-icons";
+import * as AiIcons from "react-icons/ai";
+import * as FaIcons from "react-icons/fa";
+import { useState } from "react";
+import { Data } from "./Data";
+import "./NavBar.css";
+import styled from "styled-components";
+import { SubMenu } from "./SubMenu";
+
+export function NavBar() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <div>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavIcon to="#">
+            <FaIcons.FaBars onClick={showSidebar} />
+          </NavIcon>
+        </Nav>
+        <SidebarNav sidebar={sidebar}>
+          <SidebarWrap>
+            <NavIcon to="#">
+              <AiIcons.AiOutlineClose
+                onClick={showSidebar}
+              ></AiIcons.AiOutlineClose>
+            </NavIcon>
+              <SubMenu item="1"/>
+
+              {/* {Data.map((item, index) => {
+                return <SubMenu key={index} item={item}/>
+              })} */}
+          </SidebarWrap>
+        </SidebarNav>
+      </IconContext.Provider>
+    </div>
+  );
+}
+
+const Nav = styled.div`
+  background: #15171c;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+const NavIcon = styled(Link)`
+  margin-left: 2rem;
+  font-size: 2rem;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+const SidebarNav = styled.nav`
+  background: #15171c;
+  color:white;  /* ADICION $$"$"%#%&& */
+  width: 250px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: ${({ sidebar }) => (sidebar ? "0" : "-100%")};
+  transition: 350ms;
+  z-index: 10;
+`;
+const SidebarWrap = styled.div`
+  width: 100%;
+`;
---
+++ src/Components/NavBar/SubMenu.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+
+export function SubMenu({item}) {
+  return (
+    <div>
+    </div>
+  );
+}
+
+// const SidebarLink = styled(Link)`
+//   display: flex;
+//   color: #e1e9fc;
+//   justify-content: space-between;
+//   align-items: center;
+//   padding: 20px;
+//   list-style: none;
+//   height: 60px;
+//   text-decoration: none;
+//   font-size: 18px;
+//   &:hover {
+//     background: #252831;
+//     border-left: 4px solid #632ce4;
+//     cursor: pointer;
+//   }
+// `;
